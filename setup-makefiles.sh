17b331c40abc4a97023f2426af86daf3ce2eb7f8
PlayfulGod <playfulgod@gmail.com>
2011-08-24 01:13:45 -0400

    more fixes

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE=epic
+DEVICE=indulge
 
 mkdir -p ../../../vendor/samsung/$DEVICE
 
@@ -53,7 +53,7 @@ DEVICE_PACKAGE_OVERLAYS := vendor/samsung/__DEVICE__/overlay
 \$(call inherit-product, vendor/samsung/__DEVICE__/__DEVICE__-vendor-blobs.mk)
 EOF
 
-(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/samsung/epic/BoardConfigVendor.mk
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/samsung/$DEVICE/BoardConfigVendor.mk
 # Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -81,8 +81,8 @@ BOARD_NO_32BPP := false
 
 EOF
 
-mkdir -p ../../../vendor/samsung/epic/overlay/packages/apps/Launcher2/res/layout
-(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/samsung/epic/overlay/packages/apps/Launcher2/res/layout/all_apps.xml
+mkdir -p ../../../vendor/samsung/$DEVICE/overlay/packages/apps/Launcher2/res/layout
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/samsung/$DEVICE/overlay/packages/apps/Launcher2/res/layout/all_apps.xml
 <?xml version="1.0" encoding="utf-8"?>
 <!-- Copyright (C) 2010 The Android Open Source Project
 
